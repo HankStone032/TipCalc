16f09ecc541a72f4044f850fa64ba7661e5ba001
Alexander Hagmann <ahagmann@css.edu>
2020-02-02 20:05:54 -0600

    Fixed my button and added radio buttons

--- app/src/main/java/com/example/tipcalc/MainActivity.java
+++ app/src/main/java/com/example/tipcalc/MainActivity.java
@@ -6,15 +6,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     // Declare needed variables for widgets
     EditText etBill;
-    EditText etNumPeople;
     TextView tvTotalTip;
     TextView tvTipPerPerson;
     CheckBox cbService;
+    RadioButton rb1, rb2, rb3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,27 +23,44 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         etBill = (EditText) findViewById(R.id.editTextBill);
-        etNumPeople = (EditText) findViewById(R.id.editTextNumPeople);
         tvTotalTip = (TextView) findViewById(R.id.textViewTotalTip);
-
+        tvTipPerPerson = (TextView) findViewById(R.id.textViewTipPerPerson);
         cbService = (CheckBox) findViewById(R.id.checkBoxService);
+        rb1 = (RadioButton) findViewById(R.id.radioButton1);
+        rb2 = (RadioButton) findViewById(R.id.radioButton2);
+        rb3 = (RadioButton) findViewById(R.id.radioButton3);
     }
 
 
     public void onClickButtonCalculate(View view) {
-        Double bill = Double.parseDouble(etBill.getText().toString());
-        Double numPeople = Double.parseDouble(etNumPeople.getText().toString());
-        Double totalTip;
-        if (cbService.isChecked()) {
-             totalTip = bill * 0.2;
-        } else {
-             totalTip = bill * 0.1;
+        try {
+            Double bill = Double.parseDouble(etBill.getText().toString());
+            int numPeople = 1;
+            if (rb1.isChecked()) {
+                numPeople = 1;
+            }
+            if (rb2.isChecked()) {
+                numPeople = 2;
+            }
+            if (rb3.isChecked()) {
+                numPeople = 3;
+            }
+
+            Double totalTip;
+            if (cbService.isChecked()) {
+                totalTip = bill * 0.2;
+            } else {
+                totalTip = bill * 0.1;
+            }
+
+            Double tipPerPerson = totalTip / numPeople;
+
+            tvTotalTip.setText("Total Bill: " + totalTip.toString());
+            tvTipPerPerson.setText("Tip per Person: " + tipPerPerson.toString());
         }
-
-        Double tipPerPerson = totalTip / numPeople;
-
-        tvTotalTip.setText( "Total Bill: " + totalTip.toString());
-        tvTipPerPerson.setText( "Tip per Person: " + tipPerPerson.toString());
-
+        catch(NumberFormatException ex) {
+            tvTotalTip.setText("Total Bill: 0");
+            tvTipPerPerson.setText("Tip per Person: 0");
+        }
     }
 }
